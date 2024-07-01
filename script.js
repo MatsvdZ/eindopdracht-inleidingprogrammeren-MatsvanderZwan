6ea493f4cf81b70a7c902110c90350884df93d9c
MatsvdZ <mats.van.der.zwan@hva.nl>
2024-07-02 00:10:05 +0200

    update

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ const raad = document.getElementById("raad");
 const invoeren = document.getElementById("invoeren");
 const hint = document.getElementById("hint");
 const pogingenTekst = document.getElementById("pogingen");
+const audio = new Audio();
+audio.src = "click.mp3";
 
-// Event listeners
-invoeren.addEventListener("click", controleerAntwoord);
+// Event listener, zorgt dat de knop reageert op de gebruiker zijn input en function uitvoert
+invoeren.addEventListener("click", controleerAntwoord)
 
 // controleert het getal in de input box
 function controleerAntwoord() {
     const userValue = Number(raad.value);
     pogingen = pogingen + 1; 
 
-    // Conditionals
+    // Conditionals, hiermee wordt aangegeven wanneer je het juiste getal hebt, of wanneer je te hoog of te laag zit
+    
     if(userValue === randomGetal) {
         hint.textContent = "Gefeliciteerd, hebt het juiste getal geraden!";
     } else if (userValue < randomGetal) {
